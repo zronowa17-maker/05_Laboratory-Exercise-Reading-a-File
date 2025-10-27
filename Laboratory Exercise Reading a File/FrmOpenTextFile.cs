52acd1c452dc4d04ba97a91df8228abf02000a79
zronowa17-maker <zronowa17@gmail.com>
2025-10-27 15:21:22 +0800

    add code

--- Laboratory Exercise Reading a File/FrmOpenTextFile.cs
+++ Laboratory Exercise Reading a File/FrmOpenTextFile.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Laboratory_Exercise_Reading_a_File
 {
@@ -19,7 +20,39 @@ namespace Laboratory_Exercise_Reading_a_File
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
+            lvShowText.Items.Clear();
+            openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.Title = "Select a Text File to Read";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filePath = openFileDialog1.FileName;
 
+                    string[] lines = File.ReadAllLines(filePath);
+
+                 
+                    foreach (string line in lines)
+                    {                     
+                        ListViewItem item = new ListViewItem(line);
+
+                        
+                        lvShowText.Items.Add(item);
+                    }
+                }
+                catch (IOException ex)
+                {
+                    
+                    MessageBox.Show("Error reading file: " + ex.Message, "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                  
+                    MessageBox.Show("An unexpected error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
+    
+
